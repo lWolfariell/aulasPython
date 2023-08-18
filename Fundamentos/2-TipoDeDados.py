75af0ed442ac63d416b9d33dea7edbf38d1237c1
Vitor <vitor.wolf20@gmail.com>
2023-08-18 12:40:40 -0300

    Aulas Realizadas

---
+++ Fundamentos/2-TipoDeDados.py
@@ -0,0 +1,18 @@
+"""
+Tipos em python
+
+texto = str
+Numérico = int, float, complex
+Sequência = list, tuple, range
+Mapa = dict
+Coleção = set, fronzenset
+Booleano = bool
+Binário = bytes, bytearray, memoryview
+
+ """
+
+print(11 + 10 + 1000)
+print(1.5 + 1 + 0.5)
+print(True)
+print(False)
+print('Python'+2)
---
+++ Fundamentos/3-ModoInterativos.py
@@ -0,0 +1,18 @@
+""" O modo interativo """
+
+""" 
+Para iniciar o modo interativo, chamando apenas o interpretador (python) ou executando o script com a flag -i(python -i app.py) """
+
+""" Funções dir e dir(100) """
+
+""" 
+dir - Sem argumentos, retorna a lista de nomes no escopo local atual. Com um argumento, retorna uma lista de atributos válidos para o objeto """
+
+""" 
+dir(100) """
+
+""" help
+Invoca o sistema de ajuda integrado. É possivel fazer buscas em modo interativo ou informar por parâmetro qual o nome do módulo, função, classe, método ou variável. Exemplo
+help()
+help(100) """
+
